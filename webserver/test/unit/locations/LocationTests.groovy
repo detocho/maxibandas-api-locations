a2c9b1ea8939005d968bd7395502d18f6ace423c
David Paz <dpaz@MacBook-Air-de-David.local>
2014-10-12 13:37:00 -0500

    Se agregan los test unitarios completos para la clase de location

--- webserver/test/unit/locations/LocationTests.groovy
+++ webserver/test/unit/locations/LocationTests.groovy
@@ -8,7 +8,7 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Location)
+@TestFor(locations.Location)
 class LocationTests {
 
     def registeredParentLocation
@@ -42,8 +42,33 @@ class LocationTests {
     }
 
     void test_LocationIsValidate(){
+
         assertTrue(sampleLocation.validate())
         sampleLocation.save()
-        assertEquals(sampleLocation.parentLocationId, registeredParentLocation.id)
+        assertEquals(sampleLocation.parentLocationId, registeredChildLocation.parentLocationId)
+    }
+
+    void test_LocationIsNotValidWhit_NameIsLong(){
+
+        sampleLocation.name = 'El nombre de la location es demasiado  grande y asi no se puede procesar asi que debes escribir algo mas ligero creo que falto mas longitud para poder entrar a la prueba jojojojojo'
+        assertFalse(sampleLocation.validate())
+        assertEquals('maxSize', sampleLocation.errors['name'])
+
+    }
+
+    void test_LocationIsNotValidWhit_ParentIdIsLong(){
+
+        sampleLocation.parentLocationId = 'hiduahdq34630284320874y3847184761874610984182940129874bc219984918479182740912847912874'
+        assertFalse(sampleLocation.validate())
+        assertEquals('maxSize', sampleLocation.errors['parentLocationId'])
+
+    }
+
+    void test_LocationIsNotValidWhit_NameIsBlank(){
+
+        sampleLocation.name = ''
+        assertFalse(sampleLocation.validate())
+        assertEquals('blank', sampleLocation.errors['name'])
+
     }
 }
